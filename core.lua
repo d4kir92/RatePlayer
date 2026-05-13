15f3fad5cca7343aa4e6927e7efadd1c30688875
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-05-13 12:09:42 +0200

    v1.1.110

--- core.lua
+++ core.lua
@@ -238,7 +238,7 @@ function RatePlayer:Init()
 		CLUB_FINDER_COMMUNITY_TYPE = "Community"
 	end
 
-	RatePlayer:SetVersion(135946, "1.1.109")
+	RatePlayer:SetVersion(135946, "1.1.110")
 	RAPLFrame = CreateFrame("FRAME", "RatePlayer", UIParent)
 	RAPLFrame:SetSize(iconsize * 12, iconsize * 5)
 	RAPLFrame:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 100, -100)
